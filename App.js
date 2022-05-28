3ffaec740e5a54aadd5abb834238a61cc817aaa6
Wahaha <b0464221@gmail.com>
2022-05-28 22:06:30 +0700

    Basic send and receive notification

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+// 2. Receiving and dealing the notification
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false, // Suppress the output
+      shouldSetBadge: true, // Something like red dot on unread (unhandled) E-mail
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  function scheduleNotificationHandler() {
+    // 1. Sending the notification out
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the body of the notification',
+        data: { userName: 'Max' },
+      },
+      trigger: {
+        seconds: 8,
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +42,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0C1B5E',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ myNote.md
@@ -0,0 +1,5 @@
+# Installation List
+
+1. Expo Notifications
+   Ref : https://docs.expo.dev/versions/latest/sdk/notifications/
+   expo install expo-notifications
